29ba2bd5501697334fca2bbce3188e3413298e3e
Jose <jrortegar@correo.udistrital.edu.co>
2022-01-23 03:54:40 -0500

    renderizing balance of account and available flights. We stay in video 91

--- src/App.js
+++ src/App.js
@@ -1,16 +1,56 @@
 import React, { Component } from "react";
 import Panel from "./Panel";
 import getWeb3 from "./getWeb3";
+import AirlineContract from "./airline"
+import { AirlineService } from "./airlineService";
+
+const converter = (web3) =>{
+    return(value)=>{
+        return web3.utils.fromWei(value.toString(),'ether');
+    }
+}
 
 export class App extends Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            account: undefined,
+            balance: 0,
+            flights:[]
+        }
     }
 
     async componentDidMount(){
         this.web3 = await getWeb3();
-        console.log(this.web3.version);
+        this.toEther = converter(this.web3);
+        this.airline = await AirlineContract(this.web3.currentProvider);
+        this.airlineService = new AirlineService(this.airline);
+        var account = (await this.web3.eth.getAccounts())[0];
+        this.setState({
+            account: account.toLowerCase()
+        },()=>{
+            this.load();
+        });
+    }
+
+    async getBalance(){
+        let weiBalance = await this.web3.eth.getBalance(this.state.account);
+        this.setState({
+            balance: this.toEther(weiBalance)
+        });
+    }
+
+    async getFlights(){
+        let flights = await this.airlineService.getFlights();
+        this.setState({
+            flights
+        });
+    }
+
+    async load(){
+        this.getBalance();
+        this.getFlights();
     }
 
     render() {
@@ -22,7 +62,8 @@ export class App extends Component {
             <div className="row">
                 <div className="col-sm">
                     <Panel title="Balance">
-
+                        <p><strong>{this.state.account}</strong></p>
+                        <span><strong>Balance:</strong> {this.state.balance}</span>
                     </Panel>
                 </div>
                 <div className="col-sm">
@@ -34,7 +75,11 @@ export class App extends Component {
             <div className="row">
                 <div className="col-sm">
                     <Panel title="Available flights">
-
+                        {this.state.flights.map((flight, i)=>{
+                            return <div key={i}>
+                                <span>{flight.name} - cost: {this.toEther(flight.price)}</span>
+                            </div>
+                        })}
 
                     </Panel>
                 </div>
