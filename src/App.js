9c35ccc4830c9ee9fa98267219a409549362072d
Jose <jrortegar@correo.udistrital.edu.co>
2022-01-23 02:41:42 -0500

    add web3 dependency to works in firefox

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
 import Panel from "./Panel";
+import getWeb3 from "./getWeb3";
 
 export class App extends Component {
 
     constructor(props) {
         super(props);
+    }
+
+    async componentDidMount(){
+        this.web3 = await getWeb3();
+        console.log(this.web3.version);
     }
 
     render() {
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,28 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
     mode:'development',
     entry: path.join(__dirname, "src", "index.js"),
     output: {
         path: path.resolve(__dirname, "dist"),
+    },
+    resolve: {
+        fallback: {
+            "stream": require.resolve("stream-browserify"),
+            "crypto": require.resolve("crypto-browserify"),
+            "http": require.resolve("stream-http"),
+            "https": require.resolve("http-browserify"),
+            "url": require.resolve("url/"),
+            "os": require.resolve("os-browserify/browser"),
+            "assert": require.resolve("assert/"),
+            "path": require.resolve("path-browserify"),
+            "buffer": require.resolve("buffer")
+        },
+        alias:{
+            process: "process/browser"
+        }
     },
     module: {
         rules: [
@@ -24,6 +41,10 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: path.join(__dirname, "src", "index.html"),
+            
         }),
-    ],
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.ProvidePlugin({process: "process/browser"}),
+        new webpack.ProvidePlugin({Buffer: ['buffer','Buffer']})
+    ]
 }
